f6a136f3ef2ed8f757caa8ce148cc495832f222c
clapobot team ftw <jona_blubb@web.de>
2016-10-06 13:16:42 +0000

    read tags!!!!!

--- clapobot.js
+++ clapobot.js
@@ -5,22 +5,10 @@ var SerialPort = require('serialport');
 var serialPort = new SerialPort('/dev/ttyAMA0', { baudrate: 115200 });
 var rfid = new pn532.PN532(serialPort);
 rfid.on('ready', function() {
-    rfid.getFirmwareVersion().then(function(data) {
-        console.log('firmware: ', data);
-    });
-
-    rfid.scanTag().then(function(tag) {
-        console.log('tag:', tag.uid);
-    });
     console.log('Listening for a tag scan...');
     rfid.on('tag', function(tag) {
         console.log('tag:', tag.uid);
     });
 });
 //console.log(JSON.stringify(rfid, null, 2));
-rfid.on('ready', function() {
-    rfid.scanTag().then(function(tag) {
-        console.log('tag:', tag.uid);
-    });
-});
 console.log('end');
